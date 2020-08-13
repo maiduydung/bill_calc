ce8a7b68f6e9674421e962c132160f532411ed3f
Mai Duy Dũng <maiduydung@yahoo.com>
2020-08-13 16:13:41 +0900

    added signin and register forms

--- test_app/lib/screens/authenticate/register.dart
+++ test_app/lib/screens/authenticate/register.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:test_app/services/auth.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
   final AuthSevice _auth = AuthSevice();
 
   //text field state
@@ -20,7 +20,7 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
         backgroundColor: Colors.blue[400],
         elevation: 0.0,
-        title: Text('Sign in'),
+        title: Text('Register'),
       ),
       body: Container(
           padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
@@ -44,7 +44,7 @@ class _SignInState extends State<SignIn> {
                 RaisedButton(
                   color: Colors.blue[400],
                   child: Text(
-                    'Sign in',
+                    'Register',
                     style: TextStyle(color: Colors.white),
                   ),
                   onPressed: () async {
