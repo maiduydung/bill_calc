2ca72a70029d2cebbc8db3f4402e34e861e4d757
Mai Duy Dũng <maiduydung@yahoo.com>
2020-08-28 20:01:00 +0900

    random widget

---
+++ my_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('maiduydung'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('hello world'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    ),
+
+
+  ));
+}
