c508223c3d053595bba9c37dfb2fcd9129098010
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-02 00:54:55 +0900

    added CarouselPage

--- fresh/lib/main.dart
+++ fresh/lib/main.dart
@@ -1,6 +1,7 @@
 //import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
@@ -10,15 +11,30 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "insert_name",
-          style: TextStyle(color: Colors.black38),
+        appBar: AppBar(
+          title: Text(
+            "insert_name",
+            style: TextStyle(color: Colors.black38),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.grey[200],
         ),
-        centerTitle: true,
-        backgroundColor: Colors.grey[200],
-      ),
-      body: Row(
+        body: Column(
+          children: <Widget>[CarouselPage()],
+        )
+
+        //CarouselPage()
+
+        );
+  }
+}
+
+class NavigateBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment(0.2, 1),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.values[5],
         children: <Widget>[
           IconButton(
@@ -48,6 +64,32 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class CarouselPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+          height: 200,
+          width: 600,
+          child: Carousel(
+            boxFit: BoxFit.contain,
+            dotSize: 4.0,
+            dotSpacing: 15.0,
+            dotColor: Colors.lightGreenAccent,
+            indicatorBgPadding: 5.0,
+            dotBgColor: Colors.white.withOpacity(0.5),
+            borderRadius: true,
+            images: [
+              NetworkImage(
+                  'https://images.newscientist.com/wp-content/uploads/2019/07/02111520/gettyimages-157639696.jpg'),
+              NetworkImage(
+                  'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/g9a9m-MHM425BWQ9F-Full-Image_GalleryBackground-en-US-1521579412582._SX1080_.jpg'),
+            ],
+          )),
     );
   }
 }
