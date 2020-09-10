a0b376f3665572533d5fa3e0488d049ec9a43ea3
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-11 00:03:40 +0900

    basic article screen

--- fresh/lib/screens/article.dart
+++ fresh/lib/screens/article.dart
@@ -4,29 +4,37 @@ class Article extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0),
-        child: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.grey[200],
-          actions: <Widget>[
-            new IconButton(
-                icon: new Icon(
-                  Icons.close,
-                  color: Colors.grey[600],
-                  size: 30.0,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0),
+          child: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.grey[200],
+            actions: <Widget>[
+              new IconButton(
+                  icon: new Icon(
+                    Icons.close,
+                    color: Colors.grey[600],
+                    size: 30.0,
+                  ),
+                  onPressed: () => backToMainPage(context))
+            ],
+          ),
+        ),
+        body: SafeArea(
+            child: SingleChildScrollView(
+          child: Card(
+            elevation: 0,
+            child: ListTile(
+              subtitle: Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(4),
+                child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed urna ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dapibus arcu mauris, ac luctus purus rhoncus nec. Phasellus ultrices mauris a lectus dapibus ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis eget euismod velit, eu suscipit turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec posuere lorem, quis mollis ante. Proin urna quam, fermentum ac quam sit amet, dictum mattis ipsum.",
                 ),
-                onPressed: () => backToMainPage(context))
-          ],
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-    );
+              ),
+            ),
+          ),
+        )));
   }
 }
 
