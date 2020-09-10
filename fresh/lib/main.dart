1cc3adfa78cf81146230ff9dd35f335af07cbc48
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-10 17:49:22 +0900

    remove CarouselPage

--- fresh/lib/main.dart
+++ fresh/lib/main.dart
@@ -51,6 +51,9 @@ class _HomeState extends State<Home> {
             height: 30,
           ),
           ChatCarousel(),
+          SizedBox(
+            height: 10.0,
+          ),
           NavigateBar(),
         ],
       ),
@@ -96,45 +99,5 @@ class NavigateBar extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class CarouselPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SizedBox(
-          height: 200,
-          width: 600,
-          child: Carousel(
-            boxFit: BoxFit.contain,
-            dotSize: 4.0,
-            dotSpacing: 15.0,
-            dotColor: Colors.lightGreenAccent,
-            indicatorBgPadding: 5.0,
-            dotBgColor: Colors.white.withOpacity(0.5),
-            borderRadius: true,
-            images: [
-              NetworkImage(
-                  'https://images.newscientist.com/wp-content/uploads/2019/07/02111520/gettyimages-157639696.jpg'),
-              NetworkImage(
-                  'https://images-na.ssl-images-amazon.com/images/S/sgp-catalog-images/region_US/g9a9m-MHM425BWQ9F-Full-Image_GalleryBackground-en-US-1521579412582._SX1080_.jpg'),
-            ],
-          )),
-    );
-  }
-}
-
-class Test extends StatefulWidget {
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  int counter = 1;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
