cfa2816b0f3b234f26dcb3d42740982430f2de2b
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-17 19:23:40 +0900

    2 lines of description below avatar

--- fresh/lib/screens/profile.dart
+++ fresh/lib/screens/profile.dart
@@ -28,19 +28,30 @@ class Profile extends StatelessWidget {
             height: 80.0,
             color: Colors.amber,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Column(
             children: <Widget>[
-              Container(
-                child: CircleAvatar(
-                  radius: 80.0,
-                  backgroundColor: Colors.white,
-                  child: CircleAvatar(
-                    radius: 70.0,
-                    backgroundImage: AssetImage('assets/face_1.jpg'),
+              //Avatar row, careful not to touch
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 80.0,
+                    backgroundColor: Colors.white,
+                    child: CircleAvatar(
+                      radius: 65.0,
+                      backgroundImage: AssetImage('assets/face_1.jpg'),
+                    ),
                   ),
-                ),
-              )
+                ],
+              ),
+              Text(
+                'イ二アド',
+                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              ),
+              //2 description lines below the avatar
+              Row(
+                children: <Widget>[Text('data'), Text('data')],
+              ),
             ],
           ),
         ],
