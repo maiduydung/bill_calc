921272fb877241e66a1ce0f0836826605ac25c65
Mai Duy Dũng <maiduydung@yahoo.com>
2020-09-23 17:28:24 +0900

    added profile picture at the bottom

--- fresh/lib/screens/profile.dart
+++ fresh/lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fresh/screens/navigate_bar.dart';
 
 class Profile extends StatelessWidget {
   @override
@@ -6,7 +7,7 @@ class Profile extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(30.0),
+        preferredSize: Size.fromHeight(10.0),
         child: AppBar(
           elevation: 0,
           automaticallyImplyLeading: false,
@@ -22,7 +23,109 @@ class Profile extends StatelessWidget {
           ],
         ),
       ),
-      body: Stack(
+      body: Column(
+        children: <Widget>[
+          Avatar(),
+          Text(
+            'イ二アド',
+            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          //2 description lines below the avatar
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                '11位',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+              ),
+              Text(
+                '14位',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                '週間',
+                style: TextStyle(color: Colors.yellow[700]),
+              ),
+              Text(
+                '月間',
+                style: TextStyle(color: Colors.yellow[700]),
+              )
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(60, 20, 60, 10),
+            alignment: Alignment.center,
+            child: Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+              style: TextStyle(
+                fontSize: 12.0,
+              ),
+            ),
+          ),
+          Divider(
+            height: 5,
+          ),
+          //random description below the divider
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                  child: Text(
+                    'おすすめ家電',
+                    style: TextStyle(fontSize: 17.0, color: Colors.yellow[700]),
+                  ),
+                  margin: const EdgeInsets.fromLTRB(20, 5, 0, 5)),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.fromLTRB(50, 5, 0, 5),
+                child: Text(
+                  'TV abc',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //pic at the end
+          Row(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 223,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.contain,
+                    image: NetworkImage(
+                        "https://azcd.harveynorman.com.au/media/catalog/product/q/a/qa55q80tawxxy_2.jpg"),
+                  ),
+                ),
+              )
+            ],
+          ),
+          NavigateBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class Avatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Stack(
         children: <Widget>[
           new Container(
             height: 80.0,
@@ -35,58 +138,15 @@ class Profile extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   CircleAvatar(
-                    radius: 80.0,
+                    radius: 70.0,
                     backgroundColor: Colors.white,
                     child: CircleAvatar(
-                      radius: 65.0,
+                      radius: 55.0,
                       backgroundImage: AssetImage('assets/face_1.jpg'),
                     ),
                   ),
                 ],
               ),
-              Text(
-                'イ二アド',
-                style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              //2 description lines below the avatar
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Text(
-                    '11位',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-                  ),
-                  Text(
-                    '14位',
-                    style:
-                        TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Text(
-                    '週間',
-                    style: TextStyle(color: Colors.yellow[700]),
-                  ),
-                  Text(
-                    '月間',
-                    style: TextStyle(color: Colors.yellow[700]),
-                  )
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                alignment: Alignment.center,
-                child: Text(
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-              ),
-              Divider(),
             ],
           ),
         ],
